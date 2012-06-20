57c5820bfe03b44d38fdb06f04c30514d25c9613
bmiquel <bertrand@miquel.it>
2012-06-20 23:00:56 +0200

    Version de base

---
+++ index.php
@@ -0,0 +1,151 @@
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <meta charset="utf-8">
+      <title>C.V Bertrand Miquel</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta name="description" content="">
+      <meta name="author" content="">
+
+      <!-- Le styles -->
+      <link href="css/bootstrap.min.css" rel="stylesheet">
+
+      <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
+      <!--[if lt IE 9]>
+         <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+      <![endif]-->
+
+      <!-- Le fav and touch icons -->
+      <link rel="shortcut icon" href="ico/favicon.ico">
+
+   </head>
+
+   <body data-spy="scroll" data-target=".subnav" data-offset="50">
+      
+      <div class="container">
+         <header>
+            <h1>Bertrand Miquel</h1>
+         </header>
+         
+         <section id="competence-technique">
+            <h1>Compétences techniques</h1>
+            <div class="row">
+               <div class="span6">
+                  <h2>Élaboration de programme</h2>
+                  <ul>
+                     <li>Web : PHP, HTML, CSS, Ajax, jQuery</li>
+                     <li>BDD : PL/SQL, SQL</li>
+                     <li>POO : C++, Java</li>
+                     <li>Autre : C</li>
+                  </ul>
+               </div>
+               <div class="span6">
+                  <h2>Organisation des Systèmes d'Information</h2>
+                  <ul>
+                     <li>Gestion de projets</li>
+                     <li>Rédaction de Cahier des Charges</li>
+                     <li>Plans de Reprise d'Activité</li>
+                     <li>ITIL, MEHARI</li>
+                     <li>Management des processus</li>
+                     <li>Support et formation utilisateur</li>
+                  </ul>
+               </div>
+               <div class="span6">
+                  <h2>Services Informatique</h2>
+                  <ul>
+                     <li>Réseau : DNS, DHCP, SSH, Samba</li>
+                     <li>Système : LDAP, AD </li>
+                     <li>Mail : POP, IMAP</li>
+                     <li>Web : Apache, Tomcat</li>
+                     <li>BDD : MySQL, PostgreSQL</li>
+                     <li>ToIP : Asterisk, SIP, IAX, HylaFAX</li>
+                     <li>Supervision : Nagios, Zabbix</li>
+                  </ul>
+               </div>
+               <div class="span6">
+                  <h2>Réseaux et Télécommunications</h2>
+                  <ul>
+                     <li>Routage : OSPF, RIP, Spanning Tree</li>
+                     <li>Sécurité : VLAN, chiffrement, VPN, SSL, SSID</li>
+                     <li>QOS : MPLS, 802.11</li>
+                     <li>Réseaux mobiles : 3G, GPRS, WIMAX, UMTS</li>
+                     <li>Traitement du signal, Transmission</li>
+                     <li>Administration des réseaux</li>
+                  </ul>
+               </div>
+            </div>
+         </section>
+         
+         <section id="experience-professionelle">
+            <h1>Expériences professionelles</h1>
+            <div class="row">
+               <div class="span4">
+                  <div class="date">2010 - 2011</div>
+                  <div class="societe">
+                     <a target="_blank" href="http://www.aps-prevoyance.fr/">
+                     APS Prévoyance
+                     </a>
+                  </div>
+                  <div class="ville">Avignon</div>
+                  <div class="contrat">Alternance</div>
+               </div>
+               <div class="span8">
+                  <h2>Ingénieur Réseaux et Systèmes</h2>
+                  <ul>
+                     <li>Audit du Système d'Information de l'entreprise</li>
+                     <li>Déploiement et migration des plateformes Apache / Tomcat avec SSL</li>
+                     <li>Déploiement et sécurisation d'un serveur web</li>
+                     <li>Étude et mise en place d’une solution de téléphonie Asterisk et d’HylaFAX</li>
+                     <li>Mise en place d’un système de déploiement de configurations de téléphone IP</li>
+                     <li>Rédaction de documentations et de formations utilisateurs</li>
+                  </ul>
+               </div>
+               <div class="span4">
+                  <div class="date">2009 - 2010</div>
+                  <div class="societe">
+                     <a target="_blank" href="http://dosi.univ-avignon.fr/">
+                     Université d'Avignon - D.O.S.I
+                     </a>
+                  </div>
+                  <div class="ville">Avignon</div>
+                  <div class="contrat">CDD</div>
+               </div>
+               <div class="span8">
+                  <h2>Tuteur étudiant C2i</h2>
+                  <ul>
+                     <li>Formation d'étudiants dans le cadre de l'obtention du Certificat Informatique et Internet</li>
+                     <li>Suivis de 40 étudiants sur l’année avec un volume de 180H de cours</li>
+                  </ul>
+               </div>
+               <div class="span4">
+                  <div class="date">2007 - 2009</div>
+                  <div class="societe">
+                     <a target="_blank" href="http://dosi.univ-avignon.fr/">
+                     SPIE Nucléaire
+                     </a>
+                  </div>
+                  <div class="ville">Marseille</div>
+                  <div class="contrat">CDDs et Stage</div>
+               </div>
+               <div class="span8">
+                  <h2>Technicien informatique</h2>
+                  <ul class="liste">
+                     <li>Étude de la migration d'applications Access vers un environnement « Web »</li>
+                     <li>Développement PHP / MySQL d'applications dans le cadre de la migration</li>
+                     <li>Modification des bases de données associées</li>
+                     <li>Aide informatique pour les utilisateurs</li>
+                  </ul>
+               </div>
+            
+            </div>
+         </section>
+         
+      </div>
+
+      <!-- Le javascript
+      ================================================== -->
+      <!-- Placed at the end of the document so the pages load faster -->
+      <script type="text/javascript" src="js/jquery.min.js"></script>
+      <script type="text/javascript" src="js/bootstrap.min.js"></script>
+   </body>
+</html>
